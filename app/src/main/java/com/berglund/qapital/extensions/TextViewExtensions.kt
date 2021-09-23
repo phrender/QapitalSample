182c699ee90d4018f94d58695b39c9a5a880c51c
Kevin Berglund <kevin.berglund@probisma.se>
2021-09-23 13:47:03 +0200

    minor update

--- app/src/main/java/com/berglund/qapital/extensions/TextViewExtensions.kt
+++ app/src/main/java/com/berglund/qapital/extensions/TextViewExtensions.kt
@@ -2,7 +2,9 @@ package com.berglund.qapital.extensions
 
 import android.text.Html
 import android.widget.TextView
+import androidx.core.text.HtmlCompat
 
+// TODO: Replace this
 fun TextView.setHtmlText(html: String) {
     text = Html.fromHtml(html)
 }
